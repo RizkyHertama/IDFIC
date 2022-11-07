cc30f65866901354d0d7f859abc69d7682f82e53
Farhan <animationfar@gmail.com>
2022-11-07 20:36:22 +0700

    feat: Live Chat implemented using TawkTo, and Contact Us implemented using Formspree

--- js/script.js
+++ js/script.js
@@ -31,12 +31,14 @@ navMenu();
 // counter design
 document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration){
-        let obj = document.getElementById(id),
-        current = start,
+        let obj = document.getElementById(id);
+        if(!obj) return;
+
+        let current = start,
         range = end - start,
         increment = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration /  range)),
-        timer = setInterval(() => {
+        step = Math.abs(Math.floor(duration /  range));
+        let timer = setInterval(() => {
             current += increment;
             obj.textContent = current;
             if(current == end){
